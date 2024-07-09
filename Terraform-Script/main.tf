8c43b60fd3277b22c1a5f9bae2cfa782022397a1
Syednaqiii <syed.naqi@camp2.tkxel.com>
2024-07-09 15:35:57 +0500

    Update main.tf

--- Terraform-Script/main.tf
+++ Terraform-Script/main.tf
@@ -13,22 +13,22 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-resource "tls_private_key" "rsa_4096"{
-    algorithm = "RSA"
-    rsa_bits = 4096
-}
+#resource "tls_private_key" "rsa_4096"{
+#    algorithm = "RSA"
+#    rsa_bits = 4096
+#}
 
 
-resource "aws_key_pair" "key_pair" {
-  key_name   = "DevOps-FP" #var.key_name
-  public_key = tls_private_key.rsa_4096.public_key_openssh
-}
+#resource "aws_key_pair" "key_pair" {
+#  key_name   = "DevOps-FP" #var.key_name
+#  public_key = tls_private_key.rsa_4096.public_key_openssh
+#}
 
-resource "local_file" "private_key" {
-     content = tls_private_key.rsa_4096.private_key_pem
-     filename = "DevOps-FP" #var.key_name
-  
-}
+#resource "local_file" "private_key" {
+#     content = tls_private_key.rsa_4096.private_key_pem
+#     filename = "DevOps-FP" #var.key_name
+#  
+#}
 terraform {
   backend "s3" {
     bucket = "tf-ec2-s3-backend"                 # my bucket name i.e created manually
