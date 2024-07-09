7c52e36532ded686f307fbe3a591d3522a3cd1e2
Syednaqiii <syed.naqi@camp2.tkxel.com>
2024-07-09 13:30:59 +0500

    Update main.tf

--- Terraform-Script/main.tf
+++ Terraform-Script/main.tf
@@ -20,13 +20,13 @@ resource "tls_private_key" "rsa_4096"{
 
 
 resource "aws_key_pair" "key_pair" {
-  key_name   = var.key_name
+  key_name   = "DevOps-FP" #var.key_name
   public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
 resource "local_file" "private_key" {
      content = tls_private_key.rsa_4096.private_key_pem
-     filename = var.key_name
+     filename = "DevOps-FP" #var.key_name
   
 }
 terraform {
