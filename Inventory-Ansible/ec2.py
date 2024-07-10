910dc6c1ea7cf75236aa986051df9c4588b0a22f
Syednaqiii <syed.naqi@camp2.tkxel.com>
2024-07-10 10:37:58 +0500

    Create ec2.py

---
+++ Inventory-Ansible/ec2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import boto3
+import json
+
+def get_ec2_instances():
+    ec2 = boto3.resource('ec2')
+    instances = ec2.instances.filter(
+        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    
+    inventory = {'all': {'hosts': []}}
+
+    for instance in instances:
+        inventory['all']['hosts'].append(instance.public_dns_name)
+    
+    return inventory
+
+if __name == "__main__":
+    inventory = get_ec2_instances()
+    print(json.dumps(inventory, indent=2))
