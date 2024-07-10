8ab8c370f2ec01950e6e6f6b305b76a34edbed23
Syednaqiii <syed.naqi@camp2.tkxel.com>
2024-07-10 19:32:50 +0500

    Update ec2.tf

--- Terraform-Script/ec2.tf
+++ Terraform-Script/ec2.tf
@@ -29,20 +29,15 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [
     aws_security_group.ec2_sg.id,
   ]
-
+  associate_public_ip_address = true
   tags = {
     Name = "web-server"
   }
-   #provisioner "local-exec" {
-  #command = <<-EOT
-   # INSTANCE_IP=$(aws ec2 describe-instances --instance-ids ${aws_instance.web.id} --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-   # if [ -z "$INSTANCE_IP" ]; then
-    #  echo "Failed to get instance IP. Exiting."
-   #   exit 1
-  #  fi
-  #  echo "[ec2_instances]" > $GITHUB_WORKSPACE/Inventory-Ansible/inventory.ini
-  #  echo "$INSTANCE_IP ansible_user=ubuntu ansible_ssh_private_key_file=/path/to/DevOps-FP.pem" >> $GITHUB_WORKSPACE/Inventory-Ansible/inventory.ini
-  #  ansible-playbook -i $GITHUB_WORKSPACE/Inventory-Ansible/inventory.ini $GITHUB_WORKSPACE/Ansible-Playbook/playbook.yaml
- # EOT
-#}
+     provisioner "local-exec" {
+    command = <<EOT
+      echo "[ec2_instances]" > ./Inventory-Ansible/inventory
+      echo "${aws_instance.web.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=./devOps-KP.pem" >> inventory
+      ansible-playbook -i ./Inventory-Ansible/inventory ./Ansible-Playbook/playbook.yaml
+    EOT
+  }
 }
