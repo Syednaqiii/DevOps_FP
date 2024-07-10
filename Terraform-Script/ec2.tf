4897692ba7f5c75d2722713ffce0414a28503064
Syednaqiii <syed.naqi@camp2.tkxel.com>
2024-07-10 20:45:33 +0500

    Update ec2.tf

--- Terraform-Script/ec2.tf
+++ Terraform-Script/ec2.tf
@@ -36,6 +36,7 @@ resource "aws_instance" "web" {
      provisioner "local-exec" {
     command = <<EOT
       cd ..
+      ls
       echo "[ec2_instances]" > Inventory-Ansible/inventory.ini
       echo "${aws_instance.web.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=./devOps-KP.pem" >>  Inventory-Ansible/inventory.ini
       ansible-playbook -i Inventory-Ansible/inventory.ini Ansible-Playbook/playbook.yml
